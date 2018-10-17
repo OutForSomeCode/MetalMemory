bc5fb303676f728ef7e7ce812bfdbb8256e0f2db
OutForSomeCode <43600545+OutForSomeCode@users.noreply.github.com>
2018-10-17 09:53:00 +0200

    userinterface update, begin timer

--- MetalMemory/ScoreDisplay.xaml.cs
+++ MetalMemory/ScoreDisplay.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
 
 namespace MetalMemory
 {
@@ -23,6 +24,18 @@ namespace MetalMemory
         public ScoreDisplay()
         {
             InitializeComponent();
-        }        
+        }
+
+        private string CountDownTimer()
+        {
+            int SecondsRemaining = 30;
+            Timer Timer = new Timer(1000);
+            Timer.Start();
+
+            while (SecondsRemaining != 0)
+                SecondsRemaining--;
+
+            return Timer.ToString(); 
+        } 
     }
 }
